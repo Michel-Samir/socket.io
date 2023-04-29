d1042120a393222d52ca2ef320b401dc5ceac53d
Michel-Samir <mire_samir@yahoo.com>
2023-04-29 02:38:54 +0200

    host change #2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 const port = process.env.PORT || 4000;
-const host = process.env.HOST || "localhost";
+const host = "0.0.0.0";
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
