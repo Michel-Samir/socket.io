c777ebc817c99dca3f3f6c34d263dc6110a2ee02
Michel-Samir <mire_samir@yahoo.com>
2023-04-29 02:33:32 +0200

    host change #2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 const port = process.env.PORT || 4000;
-const host = process.env.HOST || "https://socketio-production-dc15.up.railway.app/";
+const host = process.env.HOST || "localhost";
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
